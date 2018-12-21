5864c625c52afc49cedb851f08cb8f6d134d4d0e
Matt-the-Marxist <m.morrissette105000@gmail.com>
2018-12-21 12:22:43 -0500

    12/21 class

---
+++ classwork/examples/3,2,3.js
@@ -0,0 +1,31 @@
+function noOne(){
+	class themostat{
+		constructor(fahrenheit){
+			this.fahrenheit = fahrenheit;
+		}
+		get temperature(){
+			return (5/9) *(this.fahrenheit - 32);
+		}
+		set temperature(celsius){
+			this.fahrenheit = celsius *(9/5)+32;
+		}
+	}
+	
+}
+noOne();
+function noTwo(){
+	
+}
+noTwo();
+function noThree(){
+	
+}
+noThree();
+function noFour(){
+	
+}
+noFour();
+function noFive(){
+	
+}
+noFive();

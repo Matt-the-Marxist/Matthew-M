bbd7a4960564ac358d89cfabc4df3ac11b0329db
Matt-the-Marxist <matthew.morrissette105@gmail.com>
2019-02-11 13:10:02 -0500

    Update sketch.js

--- docs/personalProjects/random youtube vid/sketch.js
+++ docs/personalProjects/random youtube vid/sketch.js
@@ -1,9 +1,9 @@
-let base = "http://www.youtube.com/watch?v="
+let base = "http://www.youtube.com/watch?v=";
 
-let pullarr = ["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","-","_"]
+let pullarr = ["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","-","_"];
 
 for(let i=0; i<11; i++){
 	index = Math.random()*(pullarr.length+1);
-	base = base + pullarr[index]
+	base = base + pullarr[index];
 }
-document.write(base)
+document.write(base);

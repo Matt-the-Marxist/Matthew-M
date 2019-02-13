14938afa3ba8e52c1d75ed8180e232242fee308e
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-02-13 08:55:52 -0500

    functional

--- docs/personalProjects/random youtube vid/sketch.js
+++ docs/personalProjects/random youtube vid/sketch.js
@@ -1,5 +1,5 @@
 function randYoutubeVid(){
-	let base = "http://www.youtube.com/watch?v=";
+	let base = "";
 	let pullarr = ["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","-","_"];
 
 	for(let i=0; i<11; i++){
@@ -8,8 +8,20 @@ function randYoutubeVid(){
 	}
 	return base
 }
-function clicker(){
-	window.open(randYoutubeVid());
+function test(x){
+	let apiCheck = "https://www.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyC1XVF5CtsW7yv9K6wwgdCYMepINJBakMA&id=";
+	let request = apiCheck + x;
+	$.getJSON(request,gotData);
+
+}
+function gotData(data){
+	console.log(data.pageInfo)
+	if(data.pageInfo.totalResults > 0){
+		console.log("nice")
+	}
+	else{
+		test(randYoutubeVid());
+	}
 }
 
 //api key:AIzaSyC1XVF5CtsW7yv9K6wwgdCYMepINJBakMA

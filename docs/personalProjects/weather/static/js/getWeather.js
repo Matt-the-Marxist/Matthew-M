fe4494e54aa213ae918fbc5c2df5baa14428ea26
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-02-14 19:06:51 -0500

    clouds background and sunrise sunset

--- docs/personalProjects/weather/static/js/getWeather.js
+++ docs/personalProjects/weather/static/js/getWeather.js
@@ -1,7 +1,7 @@
 let weather;
 //gets location
 function ipLookUp () {
-	$.ajax('https://ip-api.com/json/')
+	$.ajax('http://ip-api.com/json/')
 	.then(
 		function success(response) {
 			let lat = response.lat.toString();
@@ -9,8 +9,8 @@ function ipLookUp () {
 			getWeather(lat, lon);
 		},
 		function fail(data, status) {
-			console.log('Request failed.  Returned status of',
-						status);
+			console.log('Request failed.  Returned status of',status);
+			ipLookUp();
 		}
 	);
 }
@@ -24,6 +24,7 @@ function getWeather(lat,lon) {
 	.then(
 		function success(response) {
 			weather = response;
+			console.log(weather);
 			update();
 		},
 

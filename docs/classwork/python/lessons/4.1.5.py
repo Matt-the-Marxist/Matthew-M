3d713bf03bb2f88d3368ace51f65ca443897eece
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-03-18 12:55:29 -0400

    class 3/18/19

---
+++ docs/classwork/python/lessons/4.1.5.py
@@ -0,0 +1,5 @@
+needsWater = True
+needsToBeRepotted = False
+#prints if it needs to be watered or repotted
+print("Needs water? "+str(needsWater))
+print("Needs to be repotted? "+str(needsToBeRepotted))
---
+++ docs/classwork/python/lessons/4.2.5.py
@@ -0,0 +1,8 @@
+can_juggle = bool(input("Can you juggle?(True/False)"))
+# The code below has problems. See if
+# you can fix them!
+if (can_juggle in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']):
+	print("I can juggle!")
+else:
+	print("I can't juggle.")
+

44b3939ff52ffe41aafefaeec06597543deb19f0
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-03-19 12:55:34 -0400

    class 3/19/19

--- docs/classwork/python/lessons/4.2.5.py
+++ docs/classwork/python/lessons/4.2.5.py
@@ -1,4 +1,4 @@
-can_juggle = bool(input("Can you juggle?(True/False)"))
+can_juggle = input("Can you juggle? ")
 # The code below has problems. See if
 # you can fix them!
 if (can_juggle in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']):

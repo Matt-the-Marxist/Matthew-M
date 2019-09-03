8b6418932a67ab483bb57c815e351e136a5e538c
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-09-03 13:06:06 -0400

    class 9/3/19

---
+++ docs/classwork/java/lessons/1.1.1.java
@@ -0,0 +1,7 @@
+public class helloWorld extends ConsoleProgram
+{
+	public void run()
+	{
+		System.out.println("Hello World");
+	}
+}
---
+++ docs/classwork/java/lessons/1.2.1.java
@@ -0,0 +1,2 @@
+int year = Calendar.getInstance().get(Calendar.YEAR);
+system.out.println(year);

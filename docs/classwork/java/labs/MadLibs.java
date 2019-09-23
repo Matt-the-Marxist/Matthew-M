4e351e92268d7c440f55a5eeafd2eeb5230e775c
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-09-23 12:58:17 -0400

    class 9/23/19

--- docs/classwork/java/labs/MadLibs.java
+++ docs/classwork/java/labs/MadLibs.java
@@ -5,10 +5,41 @@ public class MadLibs
     public static void main(String[] args)
     {
         Scanner s = new Scanner(System.in);
+        System.out.println("Give a Name: ");
+        String name = s.nextLine();
         
+        System.out.println("Give the pronoun matching the name\n(such as he/she/them/ect...): ");
+        String proNoun = s.nextLine();
+        
+        System.out.println("Give the possessive pronoun matching the name\n(such as his/hers/theirs/ect...): ");
+        String posProNoun = s.nextLine();
+        
+        System.out.println("Give a Dance Move: ");
+        String dance = s.nextLine();
+        
+        System.out.println("Give a verb- past-tense: ");
+        String v1 = s.nextLine();
+        
+        System.out.println("Give a verb- past-tense: ");
+        String v2 = s.nextLine();
+        
+        System.out.println("Give a verb- past-tense: ");
+        String v3 = s.nextLine();
+        
+        System.out.println("Give a verb- past-tense: ");
+        String v4 = s.nextLine();
+        
+        System.out.println("Give a verb- past-tense: ");
+        String v5 = s.nextLine();
         s.close();
+
+        System.out.println("When "+name+" went to area 51, "+proNoun+" saw an alien doing "+dance+". Not 2 minutes later, he was "+v1+" by an air force man who said \"you've seen too much.\" "+name+" smiled and said \"no u\" and the soldier "+v2+" and said \"what...?\" "+name+" "+v3+" and walked away. "+proNoun+" "+v4+" "+posProNoun+" Alien girlfriend and "+v5+" into the sunset.");
     }
 }
 
-/*when [name] went to [place], [pronown matching the name] was [adjective]
+/*
+when [Name] went to area 51, [pronoun matching name] saw an alien doing [dance move].
+not 2 minutes later, he was [verb- past-tense] by an air force man who said "you've seen too much."
+[name] smiled and said "no u" and the soldier [verb- past-tense]
+"what...?" [name] [verb past-tense] and walked away.[pronoun matching name] [verb- past-tense] [pronoun possessive] Alien girlfriend and [verb- past-tense] into the sunset.
 */

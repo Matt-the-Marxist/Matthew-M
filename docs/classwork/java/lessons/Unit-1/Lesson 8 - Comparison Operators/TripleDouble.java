9f969d3fafcccb2dd49425ddc25bc9d72a4a4294
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-09-24 12:35:58 -0400

    class 9/24/19

--- docs/classwork/java/lessons/Unit-1/Lesson 8 - Comparison Operators/TripleDouble.java
+++ docs/classwork/java/lessons/Unit-1/Lesson 8 - Comparison Operators/TripleDouble.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class TripleDouble
 {
     public static void main(String[] args) {
-        //Ask for user input here:
+        Scanner s = new Scanner(System.in);
         
-        // Create a boolean called `tripleDouble`
-        // that is true if
-        // you had at least 10 in each category
-        boolean tripleDouble = true; // replace this
+        System.out.println("Points Scored?");
+        int points = s.nextInt();
+        
+        System.out.println("# of rebounds?");
+        int rebounds = s.nextInt();
+        
+        System.out.println("# of assists?");
+        int assists = s.nextInt();
+
+
+        boolean tripleDouble = (points>=10 && rebounds>=10 && assists>=10); // replace this
         
         System.out.println("Got a Triple Double? " + tripleDouble);
+
+        s.close();
     }
 }
